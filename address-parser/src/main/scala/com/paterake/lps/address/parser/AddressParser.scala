cbea5b9017a71720b12dcb3668787c89ec8b0944
Rakesh Patel <rakesh.patel@camelotgroup.co.uk>
2020-11-11 21:11:14 +0000

    create parser using POI

--- address-parser/src/main/scala/com/paterake/lps/address/parser/AddressParser.scala
+++ address-parser/src/main/scala/com/paterake/lps/address/parser/AddressParser.scala
@@ -6,12 +6,11 @@ import org.apache.poi.ss.usermodel.{Cell, CellType, Row, Sheet}
 import org.apache.poi.xssf.usermodel.{XSSFCell, XSSFWorkbook}
 
 import scala.collection.mutable
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
 class AddressParser(inputFileName: String) {
   import scala.collection.JavaConverters._
   val inputFileStream = new FileInputStream(inputFileName)
-  val clcnHeader = new mutable.HashMap[Integer, String]()
-  val clcnTable = new mutable.HashMap[Integer, mutable.HashMap[String, String]]()
 
   def getCellValue(cell: Cell): String = {
     cell.getCellType match {
@@ -20,32 +19,37 @@ class AddressParser(inputFileName: String) {
     }
   }
 
-  def setHeader(row: Row): Unit = {
+  def getHeaderRow(row: Row): Seq[(Int, String)] = {
+    val clcnData = scala.collection.mutable.ListBuffer.empty[(Int, String)]
     row.iterator().asScala.zipWithIndex.foreach(cell => {
-      clcnHeader.put(cell._2, cell._1.getStringCellValue)
+      clcnData.append((cell._2, cell._1.getStringCellValue))
     })
+    clcnData.sortBy(_._1)
   }
 
-  def getDataRow(row: Row): mutable.HashMap[String, String] = {
-    val clcnData = new mutable.HashMap[String, String]()
+  def getDataRow(row: Row): Seq[(Int, String)] = {
+    val clcnData = scala.collection.mutable.ListBuffer.empty[(Int, String)]
     row.iterator().asScala.zipWithIndex.foreach(cell => {
-      clcnData.put(clcnHeader(cell._2), getCellValue(cell._1))
+      clcnData.append((cell._2, getCellValue(cell._1)))
     })
-    clcnData
+    clcnData.sortBy(_._1)
   }
 
   def processSheet(sheet: Sheet): Unit = {
     println(sheet.getSheetName)
+    var clcnHeader: Seq[(Int, String)] = null
+    val clcnData = scala.collection.mutable.ListBuffer.empty[(Int, Seq[(Int, String)])]
+
     sheet.iterator().asScala.zipWithIndex.foreach(row => {
       if (row._2 == 0) {
-        setHeader(row._1)
+        clcnHeader = getHeaderRow(row._1)
       } else {
-        clcnTable.put(row._2, getDataRow(row._1))
+        clcnData.append((row._2, getDataRow(row._1)))
       }
     })
     clcnHeader.foreach(f => println(f._1 + ":" + f._2))
-    clcnTable.foreach(row => {
-      println(row._1 + ":" + row._2.mkString("=>"))
+    clcnData.foreach(row => {
+      println(row._1 + ":" + row._2.mkString(";"))
     })
   }
 
