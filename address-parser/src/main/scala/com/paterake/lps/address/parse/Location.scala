c76fac7d6830dba2d9cf4bf89a757ac762089878
paterake <emailrak@gmail.com>
2021-06-07 14:59:53 +0100

    v5.3run - fix fonts for address and emails

--- address-parser/src/main/scala/com/paterake/lps/address/parse/Location.scala
+++ address-parser/src/main/scala/com/paterake/lps/address/parse/Location.scala
@@ -7,7 +7,7 @@ object Location {
   val cfgRegion = "cfgRegion"
   val nameSuffix = "/nameSuffix.txt"
   val indexNameDrop = "/indexNameDrop.txt"
-  val sourceFileName = "/home/paterake/Downloads/Master sheet V5.2 All regions Updated - For PDF production 1.6.21.xlsx"
+  val sourceFileName = "/home/paterake/Downloads/Master sheet V5.3 All regions Updated - For PDF production 4.6.21.xlsx"
   val targetFileName = "/home/paterake/Downloads/lps_draft"
   val language_parser_location = "/home/paterake/Downloads/Guju Mastersheet 22 Apr 2021.xlsx"
   val language_parser_sheet = "Correct 1-6-21 for RCP"
