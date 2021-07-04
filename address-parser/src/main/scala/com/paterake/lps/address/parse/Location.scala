a3369ca1ba0c272a4a683e8af80930a6eb68e51e
paterake <emailrak@gmail.com>
2021-07-04 17:11:54 +0100

    v5.5 run

--- address-parser/src/main/scala/com/paterake/lps/address/parse/Location.scala
+++ address-parser/src/main/scala/com/paterake/lps/address/parse/Location.scala
@@ -8,7 +8,7 @@ object Location {
   val nameSuffix = "/nameSuffix.txt"
   val indexNameDrop = "/indexNameDrop.txt"
   val addressDrop = "/addressShorten.txt"
-  val sourceFileName = "/home/paterake/Downloads/Master sheet V5.3 All regions Updated - For PDF production 4.6.21.xlsx"
+  val sourceFileName = "/home/paterake/Downloads/Master sheet V5.5 All regions Updated - For PDF production 04.7.21.xlsx"
   val targetFileName = "/home/paterake/Downloads/lps_draft"
   val language_parser_location = "/home/paterake/Downloads/Guju Mastersheet 22 Apr 2021.xlsx"
   val language_parser_sheet = "Correct 1-6-21 for RCP"
